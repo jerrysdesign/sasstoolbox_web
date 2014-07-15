d4b4427dd806a960252006bb5acec686aebecbde
jerrysdesign <jerry7722@hotmail.com>
2014-07-15 10:49:27 +0800

    menu

--- templates/project/source/javascripts/cbpTooltipMenu.js
+++ templates/project/source/javascripts/cbpTooltipMenu.js
@@ -75,7 +75,7 @@
 				}
 				else {
 					trigger.addEventListener( 'click', function( ev ) {
-						if( this.parentNode.querySelector( 'ul.cbp-tm-submenu' ) ) {
+						if( this.parentNode.querySelector( 'ul.submenu' ) ) {
 							ev.preventDefault();
 						}
 					} );
@@ -90,15 +90,15 @@
 			var self = this;
 			clearTimeout( this.omtimeout );
 			this.omtimeout = setTimeout( function() {
-				var submenu = el.querySelector( 'ul.cbp-tm-submenu' );
+				var submenu = el.querySelector( 'ul.submenu' );
 
 				if( submenu ) {
-					el.className = 'cbp-tm-show';
+					el.className = 'menu-show';
 					if( self._positionMenu( el ) === 'top' ) {
-						el.className += ' cbp-tm-show-above';
+						el.className += ' menu-show-above';
 					}
 					else {
-						el.className += ' cbp-tm-show-below';
+						el.className += ' menu-show-below';
 					}
 				}
 			}, this.touch ? 0 : this.options.delayMenu );
@@ -108,25 +108,25 @@
 			
 			clearTimeout( this.omtimeout );
 
-			var submenu = el.querySelector( 'ul.cbp-tm-submenu' );
+			var submenu = el.querySelector( 'ul.submenu' );
 
 			if( submenu ) {
 				// based on https://github.com/desandro/classie/blob/master/classie.js
-				el.className = el.className.replace(new RegExp("(^|\\s+)" + "cbp-tm-show" + "(\\s+|$)"), ' ');
-				el.className = el.className.replace(new RegExp("(^|\\s+)" + "cbp-tm-show-below" + "(\\s+|$)"), ' ');
-				el.className = el.className.replace(new RegExp("(^|\\s+)" + "cbp-tm-show-above" + "(\\s+|$)"), ' ');
+				el.className = el.className.replace(new RegExp("(^|\\s+)" + "menu-show" + "(\\s+|$)"), ' ');
+				el.className = el.className.replace(new RegExp("(^|\\s+)" + "menu-show-below" + "(\\s+|$)"), ' ');
+				el.className = el.className.replace(new RegExp("(^|\\s+)" + "menu-show-above" + "(\\s+|$)"), ' ');
 			}
 
 		},
 		_handleClick : function( el, ev ) {
 			var item = el.parentNode,
 				items = Array.prototype.slice.call( this.menuItems ),
-				submenu = item.querySelector( 'ul.cbp-tm-submenu' )
+				submenu = item.querySelector( 'ul.submenu' )
 
 			// first close any opened one..
 			if( typeof this.current !== 'undefined' &&  items.indexOf( item ) !== this.current ) {
 				this._closeMenu( this.el.children[ this.current ] );
-				this.el.children[ this.current ].querySelector( 'ul.cbp-tm-submenu' ).setAttribute( 'data-open', 'false' );
+				this.el.children[ this.current ].querySelector( 'ul.submenu' ).setAttribute( 'data-open', 'false' );
 			}
 
 			if( submenu ) {
